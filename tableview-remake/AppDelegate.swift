f0cd27f5cef10c698623579ea758197507b08cf1
Izaak Prats <izaak.prats@gmail.com>
2016-11-18 13:42:43 -0500

    Initial Commit

---
+++ tableview-remake/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tableview-remake
+//
+//  Created by Izaak Prats on 11/18/16.
+//  Copyright © 2016 IJVP. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ tableview-remake/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tableview-remake
+//
+//  Created by Izaak Prats on 11/18/16.
+//  Copyright © 2016 IJVP. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
